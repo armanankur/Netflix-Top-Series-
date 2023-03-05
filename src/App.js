f037af87e3f2e8f671954525ae0d5233bd4e04e9
Ankur <armanankur@gmail.com>
2023-03-05 12:41:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Components/Card';
+import SeriesData from './SeriesData';
 
 function App() {
+
+  const ncard=(elem)=>{
+   return(
+    <>
+    <Card 
+    imgsrc={elem.imgsrc} 
+    stitle={elem.stitle}
+    sname ={elem.sname}
+    link= {elem.link}
+    />
+    </>
+   )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='heading'>TOP NETFLIX SERIES</h1>
+     { SeriesData.map(ncard)}
     </div>
   );
 }
---
+++ src/SeriesData.js
@@ -0,0 +1,126 @@
+const SeriesData= [
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABe_W2OOOAaJoFLss63EfM5FPtm3HNFx7Q3pNdykUjoRGOr4V4vw-qJ0oOnMp9gTBKKiDgj1YgluUD4OwI_WCjZd1_w_H_Gb-BSi705yEGCWmEhneTHzfiiCbd8H0pedczBeG.jpg?r=7c1",
+        sname : 'The Last Kingdom',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80074249"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWl0JeMpEKnSA4ujLCIKoxf4EmtzTGXP97LsBMUkvBDJJ4sCsnArG3PHmxBp4B8fggtmTerz3fKfwCLEYg2iaSR_xh0dsuNy7ks8qH-531qTWtY-RcHdvKIhs_Wnni61hhLP.jpg?r=e90",
+        sname : 'Dark',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABd-iJOnLkBO7tPRFS6VNfDmHioAqvaD630vP1dKmMVTLPoz5MXFFzAxb0sqmjdUj_Q246W9vT5GIdKZym5fWiVZcFR3ldA1WfvsZiRyYSgENJ3uggxsaDGLEnJWbvhrwtXvC.jpg?r=326",
+        sname : 'Money Heist',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80192098"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWzPR8nR7Bcjz3Vgkn8zYyeOTjb5AtST4PEoxaZkbs_JlYcXyzTaNRwGa6t4UclSGnAtLiWeq7tMKIv5OqyM7dvKZLoah52bMgYuoCdxKVDnGOX7TCxP1TeQvYhwVgP79Lhm.jpg?r=501",
+        sname : 'Lucifier',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80057918"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABVQv1A6xQnfWBrmIm6iVPgryEJISEhS-hBbpxQ5sSOUftV47ER54vRNUyTq2M8p4B5AbCNMwSjGC-KrWLyU77b1Ce5JuPa_CbPYsfCU-o2J0xltM_BstcUij1-Jla3ES2VVN.jpg?r=cfe",
+        sname : 'Mismatched',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80220079"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZp_1AfaqfxDyjiE0JtKOt_EO2VFKnvvMMO_WC9-YcYiH_40u7ue8R5gVIsytjYLnS7BzEdlfzwztuQhypJs3mTS5SWYyzW9La0dfl6F8tbP-9Q3B7UpDrPFRAgUEjmdPtgq.jpg?r=4a8",
+        sname : 'Khakee : The Bihar Chapter',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/81290872"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWN2fzYdjFXJzbPSn-W2UhgO3hOzJ9K18bqEoiGV84rIzTICy721dUZagT0a4iORXvjrxbxqeBHyH87ZMF1il_BK-cRVX9bMEyyB4pePXYQX_PJGx8ChqAf1q6CVV6AJHcBr.jpg?r=e02",
+        sname : 'Little Things',
+            stitle : 'A Netflix Originals',
+            link : "https://www.netflix.com/in/title/81011159"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABaMdFkdxCiJO3YBu9pN1fFqWCzHcv_maU4LM7oDTNbvPUlWAgKyPRy1CBt3dbLb_jPXZF2xI8wDCKAFB2ZWiIKuxFFwZqab-63NpL90LcK1UyQ2QQpjeGlNjA44Lfy_Z3S-V.jpg?r=361",
+        sname : 'Kota Factory',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/81249783"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABc550ABPrq732UsbDzgbdamX3nf2OhGQ7u1bVySyfiG2E8kjofChLeCB39jateS2-xIFQ1lptQa8DmdF_I38NHemMG0dLWuZ_DSEuoHIrWl-39KjFEYC3vzKnSXAE3RA4A3-.jpg?r=df2",
+        sname : 'She',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/81183494"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfRWV3DU4RDpY_poy0SDMgj2dNfdvHh-W92WH1fRgyESphp9XiYvZ6VTcWXH0x0A7EnNB8ok6O5NXQDKGHIbvAAngELZEjnxQWClPO2ay-Lwv02fL6iozdir0PeZ7f6dkZ0H.jpg?r=592",
+        sname : 'The Witcher',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80189685"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABbMrQ93yuhpL5LITyev8yVgyRiqdF8maKzQbk4CX7tOPN_tidEVSgriNku_RON0maXtrwK8nkIraybeNCdvYQViNFWGi8K_fjszvjC7_PqbZAfPUkibY38tdgEXVSpqWbKqI.jpg?r=2f1",
+        sname : 'Manifest',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80241318"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABcMcsXlhfuklX0RkVhD7NhrBYW4blEqByiGyEro-ANU0EWBFTOk8AM-ctGRYh1kWnGrY-WArPkX6iU_zhl3Fhh52DaAvQIzBc0tKcpP_f8gaRpV7WOixShrXmrxUd0WPGPLp.jpg?r=4ca",
+        sname : 'Virgin River',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80240027"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQLmld5q2rjlY100airKKSk1EQhB4dCKjp5rAco1LfwrOwncva0yHOnCes-048j0VjXIhcRfibLX55ThfxlvmDxT5FJ3X_vvb3py6x8AClZUsvhw7tuPPm0vW5OseSn_n_fd.jpg?r=152",
+        sname : 'Lost in Space',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80104198"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABTPztrrybbafuIbVGlMXXdmX_pxTcagF3omYgI8OhRTC42EtXsAdSf3hdHP-0Llk8N1CVs790B1QT021GLGooMv2ZUzi4qJc3ADN76L7ry_fxKM_THpwn_eZakDNTd49OTXR.jpg?r=22c",
+        sname : 'Sex Education',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80197526"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABZemmrMaRP3BbvSmgKLTfo1M3yb8VgbYQQUgc3pn-Zgr4WoPJRLV4zr4Kyq6nXqGcaFbPaRcamPUw6isYOw-pDhDcnhh4crH5qlVp-eVbnQHxEGpfaH60OKmweG7cYjLz9am.jpg?r=853",
+        sname : 'Squid Game',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/81040344"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABfhk5E2MSRELjo0_aVJrPnmLUBAi13WP4CyVO8d73xbrXU9FGKGHTTH4VDKYTr1ooLR9YMTYuRGtzH2XrG8H5-Qqmtkk3viS3998iPvx3YqjBQkjRAGsAAdwr1Ps0vVmR9Eg.jpg?r=795",
+        sname : 'Elite',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/80200942"
+    },
+    {
+        imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABRZpmypZj1l1Y604YNH4suAeASU2bq-1pt8mQr2r2qQZ7iZwsBgj_koMLEpJW8DTXBmLNce8QDUi6Wi97blaL9RlG2MdDnSFSmDJSUvqrn6_0-8nKtcAIiWD_AvVYWRZAXzl.jpg?r=7f2",
+        sname : 'Jamtara',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/81183491"
+    },
+   {
+    imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXuDQfvvXT45bJAQ9mFyPupsyx9Nuby2wMGvFGA5bXDiZbTOGRqWqcToExRZlL-fOoEnUMoV3ttVJAj0clTmnbYoEBbQmgvlkgW9c1h0V2Q9gPwtyvdPVxGXwvwWCDfcUQ-a.jpg?r=fda",
+    sname : 'Dark Desire',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/81090319"
+   },
+   {
+    imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABU6o-j3nzqPAs1CaT_MyzGJBvKmkhNxU3_wgmNiyL_DCoWTamHW4Twr63NM-fwl8NWIdobQ1ennCqQbSYGKaCM0fXtzQvgSHnYIyV-sKkPGLss994yQlWcSPjFKB62v6ZEze.jpg?r=4f7",
+    sname : 'Delhi Crime',
+        stitle : 'A Netflix Originals',
+        link : "https://www.netflix.com/in/title/81076756"
+},
+
+{
+    imgsrc : "https://occ-0-2590-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQQYfeysV9QPWb4fyOgRxqtQP3PQQRVfFNCcMS1rVekQxufTJVi1-Hp65H85dbpsq_7RzRz_VvOHYKzUyfakUdYr20QuxxtBdtXIxBU0qej9bOQsi_rUsO6bhddYsnX1_q4PCP_DBhRLOmZ_IZDvpi5ZAi3bryfvBE6yChQqdX4HsvArPbTqQ-DS1Pf1zmBRVHPTS-z-lCUQqG-8lQnAXj2NUTLmvy_SojLNtU4AzA7md18cx5S9tcnAT0FprKZiqwZ81vBX7Np_8fbzHqFWDqx1sqGgZN-CqHfcQxidO6BksUiVUMQ.jpg?r=8a8",
+    sname : 'Sex/Life',
+    stitle : 'A Netflix Originals',
+    link : "https://www.netflix.com/in/title/80991848"
+},
+
+]
+
+export default SeriesData
